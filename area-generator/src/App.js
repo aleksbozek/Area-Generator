980894782340c888a2a024320c1e0be2530dc1c3
Aleks Bozek <aleksbozek@Alekss-iMac.local>
2020-09-08 10:38:37 -0400

    created first component, HomePage

--- area-generator/src/App.js
+++ area-generator/src/App.js
@@ -1,15 +1,19 @@
 import React, { useEffect, useState} from 'react';
 import axios from 'axios';
 import './App.css';
+import HomePage from './HomePage.jsx';
+
+
+
 
 function App() {
   const [build, setBuild] = useState([]);
 
   
   useEffect(() => {
-    console.log("mounted successfully")
-    // const airURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/AreaGenerator?Grid%20View`
-    const airURL = `https://api.airtable.com/v0/appT8SRRw9eOl8WmR/Build?Grid%20View`
+    
+    // const airURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/Build?Grid%20View`
+    const airURL = `https://api.airtable.com/v0/appT8SRRw9eOl8WmR/Build?Grid%20View`;
     const getInfo = async () => {
 
       const res = await axios.get(airURL, {
@@ -18,17 +22,18 @@ function App() {
         }
       });
       
-      console.log(res.data)
-      setBuild(res.data.records)
+      console.log(res.data);
+      setBuild(res.data.records);
     }
-    getInfo()
+    getInfo();
+    console.log("mounted successfully");
   }, [])
 
 
 
   return (
     <div className="App">
-      
+      <HomePage />
     </div>
   );
 }
---
+++ area-generator/src/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export default function HomePage() {
+  
+
+
+
+  return (
+
+    <div>
+
+      Stuff will go here
+       
+    </div>
+  )
+}
