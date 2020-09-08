d04f7507e8cc982db9c2b41f73864a9278ba3edf
Aleks Bozek <aleksbozek@Alekss-iMac.local>
2020-09-08 10:29:00 -0400

    grabbing airtable info via axios

--- area-generator/src/App.js
+++ area-generator/src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [build, setBuild] = useState([]);
+
+  
+  useEffect(() => {
+    console.log("mounted successfully")
+    // const airURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/AreaGenerator?Grid%20View`
+    const airURL = `https://api.airtable.com/v0/appT8SRRw9eOl8WmR/Build?Grid%20View`
+    const getInfo = async () => {
+
+      const res = await axios.get(airURL, {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+        }
+      });
+      
+      console.log(res.data)
+      setBuild(res.data.records)
+    }
+    getInfo()
+  }, [])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
