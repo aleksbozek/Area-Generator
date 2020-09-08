f7273742d486f9d4841de1988c42dcd50e2c0a13
Aleks Bozek <aleksbozek@Alekss-iMac.local>
2020-09-08 14:17:58 -0400

    gave context to data grabbed.  Moving on to the build page

--- area-generator/src/components/ShowGallery.jsx
+++ area-generator/src/components/ShowGallery.jsx
@@ -2,14 +2,24 @@ import React from "react";
 
 export default function ShowGallery(props) {
   const { Type, AssignedAttributes, Size } = props.preview.fields;
+  const timestamp = props.preview.createdTime;
 
   console.log(props);
 
   return (
     <div>
-      <h5>{Type}</h5>
-      <h5>{Size}</h5>
-      <h6>{AssignedAttributes}</h6>
+      <h5>
+        Area Style: <h6>{Type}</h6>
+      </h5>
+      <h5>
+        Size of Area: <h6>{Size}</h6>
+      </h5>
+      <h5>
+        Descriptive Keywords : <h6>{AssignedAttributes}</h6>
+      </h5>
+      <h5>Made :</h5>
+      <h6>{timestamp}</h6>
+      <br />
     </div>
   );
 }
